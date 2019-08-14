2063edff6aa05b6bc4c1f9b1835c4b041945c6df
Isacc Alvarez <ialvarez@altivasoluciones.com>
2019-08-14 17:29:59 -0600

    Cambio de construc cuentas

--- WebApp/AltivaWebApp/Controllers/CatalogoContaController.cs
+++ WebApp/AltivaWebApp/Controllers/CatalogoContaController.cs
@@ -8,8 +8,7 @@ namespace AltivaWebApp.Controllers
     using AltivaWebApp.DomainsConta;
     using AltivaWebApp.Mappers;
     using AltivaWebApp.Services;
-    using AltivaWebApp.ViewModels;
-    using System.Security.Claims;
+    using System.Collections.Generic;
 
     public class CatalogoContaController : Controller
     {
@@ -17,13 +16,13 @@ namespace AltivaWebApp.Controllers
         readonly ICatalogoContableService service;
         readonly ICatalogoContableMap map;
         readonly IBitacoraMapper bitacoraMap;
-       
+
         public CatalogoContaController(ICatalogoContableService service, ICatalogoContableMap map, IBitacoraMapper bitacoraMap, BaseConta _bd)
         {
             this.service = service;
             this.map = map;
             this.bitacoraMap = bitacoraMap;
-            this.bd = _bd;
+            bd = _bd;
         }
         public IActionResult Index()
         {
@@ -31,110 +30,188 @@ namespace AltivaWebApp.Controllers
             ViewBag.CatalogoContable = bd.CatalogoContable.ToList();
             return View();
         }
-        public ActionResult u(long id)
+        private void initViewBag()
         {
             ViewBag.Titulo = "setCatalogoConta";
+            ViewBag.Error = false;
             ConfiguracionContable c = bd.ConfiguracionContable.FirstOrDefault();
             ViewBag.Formato = c.Ejemplo;
+            ViewBag.Min = 1;
+            ViewBag.Max = 6;
+            ViewBag.Ceros = 1;
+
+        }
+        public ActionResult nItem(long idP)
+        {
+            initViewBag();
             AltivaWebApp.ViewModels.CatalogoContableViewModel dato;
-            if (id == 0)
+            dato = new AltivaWebApp.ViewModels.CatalogoContableViewModel();
+            AltivaWebApp.DomainsConta.CatalogoContable padre;
+
+            padre = service.GetCatalogoContableById(idP);
+            if (padre != null)
             {
 
-                dato = new AltivaWebApp.ViewModels.CatalogoContableViewModel();
+                dato.Nivel = Nivel(padre);
+                dato.CuentaContable = padre.CuentaContable;
+                dato.CuentaContablePadre = padre.CuentaContablePadre;
+                dato.DescCuentaPadre = padre.DescCuentaPadre;
+                dato.IdCuentaContablePadre = padre.IdCuentaContable;
+                ViewBag.Min = 1;
+                ViewBag.Max = MaxHija(padre);
+                ViewBag.Espacios = EspaciosNivel(dato.Nivel);
             }
-            else {
-                dato = map.DomainToViewModel(service.GetCatalogoContableById(id));
-            }            
+            else
+            {
+                dato.Nivel = 1;
+                ViewBag.Min = 1;
+                ViewBag.Max = 6;
+                ViewBag.Espacios = 1;
+
+            }
+            return View("../CatalogoConta/u", dato);
+        }
+        public ActionResult eItem(long id)
+        {
+            initViewBag();
+            AltivaWebApp.ViewModels.CatalogoContableViewModel dato;
+            dato = map.DomainToViewModel(service.GetCatalogoContableById(id));
 
             return View("../CatalogoConta/u", dato);
-
-            //return View(u);
         }
-        public IActionResult uDet(long id)
+        private short Nivel(CatalogoContable _p)
         {
-            ViewBag.Titulo = "setCatalogoConta";
+
+            short siguiente = (short)(_p.Nivel + 1);
+            return siguiente;
+        }
+        private int EspaciosNivel(int n)
+        {
             ConfiguracionContable c = bd.ConfiguracionContable.FirstOrDefault();
-            ViewBag.Formato = c.Ejemplo;
+            if (n == 1)
+            {
 
-            var padre = bd.CatalogoContable.Find(id);
-            var d = new DomainsConta.CatalogoContable();
-            d.IdCuentaContablePadre = padre.IdCuentaContable;
-            d.CuentaContablePadre = padre.CuentaContable;
-            d.CuentaContable = padre.CuentaContable;
-            d.DescCuentaPadre = padre.Descripcion;
-            return View("u", d);
+                return 1;
+            }
+            if (p.Nivel == 2)
+            {
+
+                return c.Nivel2;
+            }
+            if (p.Nivel == 3)
+            {
+
+                return c.Nivel3;
+            }
+            if (p.Nivel == 4)
+            {
+
+                return c.Nivel4;
+            }
+            if (p.Nivel == 5)
+            {
+
+                return c.Nivel5;
+            }
+            if (p.Nivel == 5)
+            {
+
+                return c.Nivel5;
+            }
+            if (p.Nivel == 6)
+            {
+
+                return c.Nivel6;
+            }
+            if (p.Nivel == 7)
+            {
+
+                return c.Nivel7;
+            }
+            if (p.Nivel == 8)
+            {
+                return c.Nivel8;
+            }
+            return 1;
         }
-  
+        private int MaxHija(CatalogoContable _p)
+        {                     
+            if (p.Nivel == 1)
+            {
 
-        [HttpPost("gU")]
-        public JsonResult gU(CatalogoContableViewModel model)
+                return 6;
+            }
+            else {
+                return digitosDisponibles(EspaciosNivel(p.Nivel));
+            }
+        }
+        private int digitosDisponibles(int cant)
         {
-            try
+            string nueves = "";
+
+            for (int i = 0; i < cant; i++)
             {
-
-                var existeCuenta = service.GetCatalogoContableByNombre(model.Descripcion);
-                if (model.IdCuentaContable != 0)
-                {
-                    if (existeCuenta != null)
-                        if ((int)existeCuenta.IdCuentaContable != model.IdCuentaContable)
-                            return Json(new { success = false });
-
-                    var catalogoCuenta = map.Update(model, model.IdCuentaContable);
-                    var idUsuario = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
-
-                    string comentarioES = "Editó la cuenta contable " + catalogoCuenta.Descripcion;
-
-                    bitacoraMap.CrearBitacora(int.Parse(idUsuario), comentarioES, (int)catalogoCuenta.IdCuentaContable, "CatalogoCuentaContable");
-
-                    return Json(new { success = true });
-                }
-                else
-                {
-                    if (existeCuenta != null)
-                        return Json(new { success = false });
-
-                    var catalogoCuenta = map.Create(model);
-                    var idUsuario = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
-                    string comentarioES = "Creo una cuenta contable " + catalogoCuenta.Descripcion;
-                    //string comentarioIN = "Creo una nueva bitacora";
-                    bitacoraMap.CrearBitacora(int.Parse(idUsuario), comentarioES, (int)catalogoCuenta.IdCuentaContable, "CatalogoCuentaContable");
-                    return Json(new { success = true });
-
-                }
-
+                nueves = nueves + "9";
 
             }
-            catch
-            {
-                //throw;
-                return   Json(new { success = false });
-            }
+            return int.Parse(nueves);
         }
-
-
         [BindProperty]
         public CatalogoContable p { get; set; }
-        public IActionResult guardar()
+        public ActionResult guardar()
         {
-            bool existe = false;
-
+            initViewBag();
             if (!ModelState.IsValid)
             {
                 return RedirectToAction("Index");
             }
+            //Validaciones
+            List<CatalogoContable> concidencias = bd.CatalogoContable.Where(x => x.CuentaContable == p.CuentaContable && x.IdCuentaContable != p.IdCuentaContable).ToList();
+            if (concidencias.Count > 0)
+            {
+                ViewBag.Error = true;
+                ViewBag.Dato = "Cuenta";
+
+            }
+            concidencias = bd.CatalogoContable.Where(x => x.Descripcion == p.Descripcion && x.IdCuentaContable != p.IdCuentaContable).ToList();
+            if (concidencias.Count > 0)
+            {
+                ViewBag.Error = true;
+                ViewBag.Dato = "Descripcion";
+
+            }
+
+            if (p.Movimiento == true && p.CuentaContablePadre == "")
+            {
+                ViewBag.Error = true;
+                ViewBag.Dato = "MovimientoInvalido";
+
+            }
+            concidencias = bd.CatalogoContable.Where(x => x.IdCuentaContablePadre == p.IdCuentaContable).ToList();
+            if (concidencias.Count > 0 && p.Movimiento)
+            {
+                ViewBag.Error = true;
+                ViewBag.Dato = "MovimientoInvalido";
+
+            }
+
+            if (ViewBag.Error)
+            {
+                var dato = map.DomainToViewModel(p);
+                return View("../CatalogoConta/u", dato);
+            }
+
             var _Cambios = bd.CatalogoContable.Where(x => x.IdCuentaContable == p.IdCuentaContable).SingleOrDefault();
             if (_Cambios == null)
             {
+
                 _Cambios = new CatalogoContable();
                 bd.CatalogoContable.Add(_Cambios);
             }
-            else {
 
-               bd.CatalogoContable.Where(x => x.Descripcion == p.Descripcion && x.IdCuentaContable != _Cambios.IdCuentaContable).SingleOrDefault();
-                if (_Cambios != null) {
-                    return RedirectToAction("Index");
-                }
-            }
+
+
+
             _Cambios.CuentaContable = p.CuentaContable;
             _Cambios.Descripcion = p.Descripcion;
             _Cambios.Nivel = p.Nivel;
