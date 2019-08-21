e4b94f656b4d7c04a71c4c773196d8828ea30053
Isacc Alvarez <ialvarez@altivasoluciones.com>
2019-08-21 09:50:49 -0600

    correciones de combinar

--- WebApp/AltivaWebApp/Controllers/BodegaController.cs
+++ WebApp/AltivaWebApp/Controllers/BodegaController.cs
@@ -4,6 +4,7 @@ using AltivaWebApp.Services;
 using AltivaWebApp.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
