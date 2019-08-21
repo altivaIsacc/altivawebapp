93d676f56554b1d2faf213498b443acf76428061
desarrollopymes@altivasoluciones.com <nueva2019+>
2019-08-21 14:33:33 -0600

    Cambios no publicados

--- WebApp/AltivaWebApp/Controllers/GastoController.cs
+++ WebApp/AltivaWebApp/Controllers/GastoController.cs
@@ -103,7 +103,7 @@ namespace AltivaWebApp.Controllers
                             if (model2.Count() > 0)
                             {
                                 viewModel.ComprasDetalleServicio = model2;
-                                var cds = map.UpdateCDS(viewModel);
+                                //var cds = map.UpdateCDS(viewModel);
 
                             }
                         }
