bc510b83c46bf0162b8c84c47b3e32ab12f0abff
Ignacio Solano <asistentesoporte@altivasoluciones.com>
2019-09-03 08:29:26 -0600

    Cambios factura

--- WebApp/AltivaWebApp/Controllers/FacturaController.cs
+++ WebApp/AltivaWebApp/Controllers/FacturaController.cs
@@ -8,6 +8,7 @@ using AltivaWebApp.Mappers;
 using AltivaWebApp.Services;
 using AltivaWebApp.ViewModels;
 using FastReport.Export.Image;
+using FastReport.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -47,19 +48,19 @@ namespace AltivaWebApp.Controllers
         public IActionResult EditarFactura(long id)
         {
 
-            //FastReport.Utils.Config.WebMode = true;
-            //var rep = new WebReport();
-            //var savePath = System.IO.Path.Combine(Startup.entorno.WebRootPath, "Reportes");
-            //var path = $"{savePath}\\Facturaticket.frx";
+            FastReport.Utils.Config.WebMode = true;
+            var rep = new WebReport();
+            var savePath = System.IO.Path.Combine(Startup.entorno.WebRootPath, "Reportes");
+            var path = $"{savePath}\\Facturaticket.frx";
 
 
-            //rep.Report.Load(path);
+            rep.Report.Load(path);
 
-            //rep.Report.Dictionary.Connections[0].ConnectionString = StringProvider.StringEmpresas;
-            //rep.Report.SetParameterValue("idFactura", id);
-            //rep.Report.Prepare();
+            rep.Report.Dictionary.Connections[0].ConnectionString = StringProvider.StringEmpresas;
+            rep.Report.SetParameterValue("idFactura", id);
+            rep.Report.Prepare();
 
-            //ViewBag.reporte = rep;
+            ViewBag.reporte = rep;
 
             ViewData["usuarios"] = userService.GetAllByIdEmpresa((int)HttpContext.Session.GetInt32("idEmpresa"));
             ViewData["clientes"] = contactoService.GetAllClientes();
@@ -70,75 +71,73 @@ namespace AltivaWebApp.Controllers
         public IActionResult GetTicket(long idFactura)
         {
 
-            //FastReport.Utils.Config.WebMode = true;
-            //var rep = new WebReport();
-            //var savePath = System.IO.Path.Combine(Startup.entorno.WebRootPath, "Reportes");
-            //var path = $"{savePath}\\Facturaticket.frx";
+            FastReport.Utils.Config.WebMode = true;
+            var rep = new WebReport();
+            var savePath = System.IO.Path.Combine(Startup.entorno.WebRootPath, "Reportes");
+            var path = $"{savePath}\\Facturaticket.frx";
 
-            //rep.Report.Load(path);
-
-
-            //rep.Report.Dictionary.Connections[0].ConnectionString = StringProvider.StringEmpresas;
-            //rep.Report.Dictionary.Connections[1].ConnectionString = StringProvider.StringGE;
-            //rep.Report.SetParameterValue("idFactura", idFactura);
-            //rep.Report.SetParameterValue("idEmpresa", HttpContext.Session.GetInt32("idEmpresa"));
+            rep.Report.Load(path);
 
 
-            //if (rep.Report.Prepare())
-            //{
-            //    FastReport.Export.Image.ImageExport imgExport = new FastReport.Export.Image.ImageExport();
-            //    //imgExport.ShowProgress = false;
-            //    imgExport.ImageFormat = FastReport.Export.Image.ImageExportFormat.Jpeg;
-            //    imgExport.SeparateFiles = false;
-            //    imgExport.Resolution = 300;
+            rep.Report.Dictionary.Connections[0].ConnectionString = StringProvider.StringEmpresas;
+            rep.Report.Dictionary.Connections[1].ConnectionString = StringProvider.StringGE;
+            rep.Report.SetParameterValue("idFactura", idFactura);
+            rep.Report.SetParameterValue("idEmpresa", HttpContext.Session.GetInt32("idEmpresa"));
 
-            //    MemoryStream strm = new MemoryStream();
-            //    rep.Report.Export(imgExport, strm);
-            //    rep.Report.Dispose();
-            //    imgExport.Dispose();
-            //    strm.Position = 0;
 
-            //    return File(strm, "image/jpeg", "report.pdf");
-            //}
-            //else
-            //{
-            //    return null;
-            //}
-            return null;
+            if (rep.Report.Prepare())
+            {
+                FastReport.Export.Image.ImageExport imgExport = new FastReport.Export.Image.ImageExport();
+                //imgExport.ShowProgress = false;
+                imgExport.ImageFormat = FastReport.Export.Image.ImageExportFormat.Jpeg;
+                imgExport.SeparateFiles = false;
+                imgExport.Resolution = 300;
+
+                MemoryStream strm = new MemoryStream();
+                rep.Report.Export(imgExport, strm);
+                rep.Report.Dispose();
+                imgExport.Dispose();
+                strm.Position = 0;
+
+                return File(strm, "image/jpeg", "report.pdf");
+            }
+            else
+            {
+                return null;
+            }
 
 
         }
 
         public IActionResult GetTicketImage()
         {
-            //// Creatint the Report object
+            // Creatint the Report object
 
-            //FastReport.Utils.Config.WebMode = true;
-            //var report = new WebReport();
-            //var savePath = System.IO.Path.Combine(Startup.entorno.WebRootPath, "Reportes");
-            //var path = $"{savePath}\\Facturaticket.frx";
-
-
-            //report.Report.Load(path);
+            FastReport.Utils.Config.WebMode = true;
+            var report = new WebReport();
+            var savePath = System.IO.Path.Combine(Startup.entorno.WebRootPath, "Reportes");
+            var path = $"{savePath}\\Facturaticket.frx";
 
 
-            //report.Report.Prepare();// Preparing a report
+            report.Report.Load(path);
 
-            //// Creating the Image export
-            //using (ImageExport image = new ImageExport())
-            //{
-            //    image.ImageFormat = ImageExportFormat.Jpeg;
-            //    image.JpegQuality = 100; // Set up the quality
-            //    image.Resolution = 100; // Set up a resolution 
-            //    image.SeparateFiles = false; // We need all pages in one big single file
 
-            //    using (MemoryStream st = new MemoryStream())// Using stream to save export
-            //    {
-            //        report.Report.Export(image, st);
-            //        return base.File(st.ToArray(), "image/jpeg");
-            //    }
-            //}
-            return null;
+            report.Report.Prepare();// Preparing a report
+
+            // Creating the Image export
+            using (ImageExport image = new ImageExport())
+            {
+                image.ImageFormat = ImageExportFormat.Jpeg;
+                image.JpegQuality = 100; // Set up the quality
+                image.Resolution = 100; // Set up a resolution 
+                image.SeparateFiles = false; // We need all pages in one big single file
+
+                using (MemoryStream st = new MemoryStream())// Using stream to save export
+                {
+                    report.Report.Export(image, st);
+                    return base.File(st.ToArray(), "image/jpeg");
+                }
+            }
 
         }
 
