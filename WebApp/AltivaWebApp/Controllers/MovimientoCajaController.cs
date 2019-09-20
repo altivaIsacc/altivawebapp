32b48f279be562a661fd76a477fc81d995f7f81d
Ignacio Solano <asistentesoporte@altivasoluciones.com>
2019-09-19 18:00:54 -0600

    reporte personalizado factura
    Inicio de pago en efectivo

---
+++ WebApp/AltivaWebApp/Controllers/MovimientoCajaController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AltivaWebApp.Services;
+using AltivaWebApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AltivaWebApp.Controllers
+{
+    [Route("{culture}/MovimientoCaja")]
+    public class MovimientoCajaController : Controller
+    {
+        private readonly IDenominacionesService denService;
+
+        public MovimientoCajaController(IDenominacionesService denService)
+        {
+            this.denService = denService;
+        }
+
+        [HttpPost("_FormaPago")]
+        public IActionResult _FormaPago(FormaPagoViewModel viewModel)
+        {
+            ViewData["denominaciones"] = denService.GetAllDenominaciones();
+            return PartialView(viewModel);
+        }
+    }
+}
---
+++ WebApp/AltivaWebApp/ViewModels/FormaPagoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AltivaWebApp.ViewModels
+{
+    public class FormaPagoViewModel
+    {
+        public int IdCaja { get; set; }
+        public double Monto { get; set; }
+        public double TcDolar { get; set; }
+        public double TcEuro { get; set; }
+        public int Moneda { get; set; }
+    }
+}
