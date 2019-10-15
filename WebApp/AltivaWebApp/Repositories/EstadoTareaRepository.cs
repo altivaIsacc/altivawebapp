dd1225695e511f5e5174337178e958fa09847393
desarrolloPymes <49493745+desarrolloPymes@users.noreply.github.com>
2019-10-15 17:45:36 -0600

    Ultima actualizacion del modulo estados de tareas
    Tareas: El sistema permite tener como valor inicial un estado inactivas

--- WebApp/AltivaWebApp/Repositories/EstadoTareaRepository.cs
+++ WebApp/AltivaWebApp/Repositories/EstadoTareaRepository.cs
@@ -12,54 +12,69 @@ namespace AltivaWebApp.Repositories
         {
         }
 
-        public bool GetByColor(string color)
+        // metodos utilizados cuando edita
+
+        public TbFdTareaEstado GetTitulo(string titulo) // edita 1
+        {
+            return context.TbFdTareaEstado.FirstOrDefault(i => i.Titulo == titulo);
+        }
+
+        public TbFdTareaEstado GetColor(string color) //edita 2
+        {
+            return context.TbFdTareaEstado.FirstOrDefault(i => i.Color == color);
+        }
+        public TbFdTareaEstado GetDefecto(bool flag) //edita 3
+        {
+            return context.TbFdTareaEstado.FirstOrDefault(i => i.EsDefecto == flag);
+        }
+        public TbFdTareaEstado GetInicial(bool flag) //edita 4
+        {
+            return context.TbFdTareaEstado.FirstOrDefault(i => i.EsInicial == flag);
+        }
+        public TbFdTareaEstado GetFinal(bool flag) //edita 5
+        {
+            return context.TbFdTareaEstado.FirstOrDefault(i => i.EsFinal == flag);
+        }
+
+
+
+        // metodos utilizados cuando edita
+        public bool GetByTitulo(string titulo) //edita
+        {
+            return context.TbFdTareaEstado.Any(u => u.Titulo == titulo);
+        }
+
+        public bool GetByColor(string color) //edita
         {
             return context.TbFdTareaEstado.Any(u => u.Color == color && u.Color != "#858585");
         }
 
-        public bool GetByDefecto(bool? defecto)
+        public bool GetByDefecto(bool? defecto) // edita
         {
             return context.TbFdTareaEstado.Any(u => u.EsDefecto == defecto);
         }
 
-        public bool GetByEsInicial(bool? inicial)
+        public bool GetByEsInicial(bool? inicial) //edita
         {
             return context.TbFdTareaEstado.Any(u => u.EsInicial == inicial);
         }
-
-        public bool GetByEsFinal(bool? final)
+         
+        public bool GetByEsFinal(bool? final) //edita
         {
             return context.TbFdTareaEstado.Any(u => u.EsFinal == final);
         }
+
+
+
+
 
         public TbFdTareaEstado GetById(int idEstado)
         {
             return this.context.TbFdTareaEstado.Where(te => te.Id == idEstado).FirstOrDefault();
         }
 
-        public bool GetByTitulo(string titulo)
-        {
-            return context.TbFdTareaEstado.Any(u => u.Titulo == titulo);
-        }
-        public TbFdTareaEstado GetColor(string color)
-        {
-            return context.TbFdTareaEstado.FirstOrDefault(i => i.Color == color );
-        }
-        public TbFdTareaEstado GetDefecto(bool flag)
-        {
-            return context.TbFdTareaEstado.FirstOrDefault(i => i.EsDefecto == flag);
-        }
-        public TbFdTareaEstado GetInicial(bool flag)
-        {
-            return context.TbFdTareaEstado.FirstOrDefault(i => i.EsInicial == flag);
-        }
-        public TbFdTareaEstado GetFinal(bool flag)
-        {
-            return context.TbFdTareaEstado.FirstOrDefault(i => i.EsFinal == flag);
-        }
-        public TbFdTareaEstado GetTitulo(string titulo)
-        {
-            return context.TbFdTareaEstado.FirstOrDefault(i => i.Titulo == titulo);
-        }
+      
+
+   
     }
 }
